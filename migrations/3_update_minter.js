0d745bab8109be8082c7bb5a2e198799714eb1c6
Phillip Birtcher <philbirt@philbirt.com>
2018-10-09 11:42:12 -0400

    Collapse migration 2 + 3

--- migrations/3_update_minter.js
+++
@@ -1,6 +0,0 @@
-const CryptoCareMinter = artifacts.require("./CryptoCareMinter.sol");
-
-module.exports = async function(deployer) {
-  const minterInstance = await CryptoCareMinter.deployed();
-  await minterInstance.updateMinter('0x8a9df80fa754ea6cf7241af654685c21a87af22e');
-};
